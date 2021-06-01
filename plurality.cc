47ca661eaf4957b49cc7bdc8904c979fd09ef350
Macbook Pro <joserodriguez@joses-mbp.attlocal.net>
2021-05-31 20:09:57 -0700

    c++ version

--- plurality.cc
+++ plurality.cc
@@ -3,21 +3,13 @@
 using namespace std;
 
 // plurality vote, every voter chooses one candidate
-// ./plurality Alice Bob Charlie
+// ./plurality < name1, name2, name3 ... >
 // Outputs the winner
 
+// Max number of candidates
 #define MAX 9
 
-// Number of candidates
-int candidate_count;
-int adding_to_file; // added via github
-
-// Function Prototypes
-bool vote(string name);
-void print_winner(void);
-
-int delete_later; // delete now! now!
-
+// Candidates name and vote count
 typedef struct {
     string name;
     int votes;
@@ -25,6 +17,13 @@ typedef struct {
 
 // Array of candidates
 candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function Prototypes
+bool vote(string name);
+void print_winner(void);
 
 int main(int argc, const char * argv[])
 {
@@ -35,30 +34,22 @@ int main(int argc, const char * argv[])
         return 1;
     }
 
+    // Populating array of candidates
     candidate_count = argc - 1;
-
     if (candidate_count > MAX)
     {
         cout << "Maximum number of candidates is " << MAX << endl;
         return 2;
     }
-
     for (int i = 0; i < candidate_count; i++)
     {
         candidates[i].name = argv[i + 1];
         candidates[i].votes = 0;
     }
 
-    
-    for(int i = 1; i < argc; ++i){
-        cout << argv[i] << ", ";
-    }
-    cout << endl;
-
     int voter_count;
     cout << "Number of voters: ";
     cin >> voter_count;
-
 
     // Loop over all voters
     for (int i = 0; i < voter_count; i++)
@@ -71,10 +62,10 @@ int main(int argc, const char * argv[])
         if (!vote(name))
         {
             cout << "Invalid Vote" << endl;
-            --i;
         }
     }
 
+    // Display the winner of the election
     print_winner();
 
     return 0;
@@ -86,30 +77,27 @@ bool vote(string name){
 
     for(int i = 0; i < candidate_count; i++){
         if(candidates[i].name == name)
+        {
+            cout << "inside vote loop" << endl;
             found = true;
-        if(found){
-            candidates[i].votes += 1;
-            cout << candidates[i].votes;
+            candidates[i].votes = candidates[i].votes + 1;
+            return found;
         }
     }
     return found;
 }
 
 void print_winner(void){
-    int highest_vote;
-    string winner [candidate_count];
+    int highest_vote = 0;
 
     for(int i = 0; i < candidate_count; i++){
-        for(int j = 1; j < candidate_count; j++){
-            if(candidates[i].votes > candidates[j].votes)
-                highest_vote = candidates[i].votes;
-        }
+        if(highest_vote < candidates[i].votes)
+            highest_vote = candidates[i].votes;
     }
     cout << highest_vote << endl;
     cout<< "Winner:" << endl;
     for(int i = 0; i < candidate_count; i++){
-        if(highest_vote == candidates[i].votes){
+        if(highest_vote == candidates[i].votes)
             cout << candidates[i].name << endl;
-        }
     }
 }
